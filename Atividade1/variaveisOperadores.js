4dc9f9cccbf4de1c012685d988192dbb7afcc331
Gustavo Barbosa <gustavobbarbosa4@gmail.com>
2026-03-06 21:11:49 -0300

    Atividade 1 feito

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,13 @@
+let num1 = 10
+let num2 = 5
+
+let soma  = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+let resto = num1 %  num2  
+
+console.log(`A soma entre ${num1} e ${num2} é: ${soma}`)
+console.log(`A Subtração entre ${num1} e ${num2} é: ${subtracao}`)
+console.log(`A multiplicação entre ${num1} e ${num2} é: ${multiplicacao}`)
+console.log(`A divisão entre ${num1} e ${num2} é: ${divisao} e o resto é ${resto}`)
